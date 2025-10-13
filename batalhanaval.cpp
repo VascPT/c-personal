a3bb9a01866ce825d01b08d58e18c4fda8f774a8
Vasco <vascorf2008@gmail.com>
2025-10-13 19:06:02 +0000

    .\.vscode\

--- batalhanaval.cpp
+++ batalhanaval.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
+void portaaviao(){
+    cout << endl << "***** ***** ***** ***** *****" << endl;
+    cout << "*   * *   * *   * *   * *   *" << endl;
+    cout << "***** ***** ***** ***** *****" << endl;
+
+}   
+void cruzador(){
+    cout << endl << "***** ***** ***** *****" << endl;
+    cout<< "*   * *   * *   * *   *" << endl;
+    cout<< "***** ***** ***** *****" << endl;
+}
+void contratorpedeiro(){
+    cout << endl << "***** ***** *****" << endl;
+    cout<< "*   * *   * *   *" << endl;
+    cout<< "***** ***** *****" << endl;
+}
+void rebocador(){
+    cout << endl << "***** *****" << endl;
+    cout<< "*   * *   *" << endl;
+    cout<< "***** *****" << endl;
+}
+
+int main(){
+    system("CLS");
+    portaaviao();
+    cruzador();
+    contratorpedeiro();
+    rebocador();
+
+    return 0;
+}
