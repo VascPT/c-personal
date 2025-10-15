33cda5a603b1cf8aa9aa8c99ea4db00eb10ed86f
Vasco <vascorf2008@gmail.com>
2025-10-15 21:57:24 +0000

    .\.vscode\settings.json

--- batalhanaval.cpp
+++ batalhanaval.cpp
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
 using namespace std;
 
 void barco5H(){
-    cout << "BBBBB" << endl;
+    cout << "B B B B B" << endl;
 }
 void barco5V(){
     cout << "B" << endl;
@@ -15,7 +16,7 @@ void barco5V(){
     cout << "B" << endl;
 }
 void barco4H(){
-    cout << "BBBB" << endl;
+    cout << "B B B B" << endl;
 }
 void barco4V(){
     cout << "B" << endl;
@@ -24,7 +25,7 @@ void barco4V(){
     cout << "B" << endl;
 }
 void barco3H(){
-    cout << "BBB" << endl;
+    cout << "B B B" << endl;
 }
 void barco3V(){
     cout << "B" << endl;
@@ -32,7 +33,7 @@ void barco3V(){
     cout << "B" << endl;
 }
 void barco2H(){
-    cout << "BB" << endl;
+    cout << "B B" << endl;
 }
 void barco2V(){
     cout << "B" << endl;
@@ -53,7 +54,7 @@ struct Barco{
 
 void printBoard(char board[N][N], bool showShips) {
     cout << "   ";
-    for (int c = 1; c <= N; c++) cout << c << " ";
+    cout << "A B C D E F G H I J";
     cout << "\n";
 
     for (int r = 0; r < N; r++) {
@@ -65,7 +66,12 @@ void printBoard(char board[N][N], bool showShips) {
         cout << "\n";
     }
 }
+
 Barco barcoCinco;
+Barco barcoQuatro;
+Barco barcoTres;
+Barco barcoDois;
+
 
 void barco5VIEW(Barco barcoCinco){
     if (barcoCinco.horizontal == true){
@@ -74,21 +80,21 @@ void barco5VIEW(Barco barcoCinco){
         barco5V();
     }
 }
-void barco4VIEW(Barco &barcoQuatro){
+void barco4VIEW(Barco barcoQuatro){
     if(barcoQuatro.horizontal == true){
         barco4H();
     }else if(barcoQuatro.horizontal == false){
         barco4V();
     }
 }
-void barco3VIEW(Barco &barcoTres){
+void barco3VIEW(Barco barcoTres){
     if(barcoTres.horizontal == true){
         barco3H();
     }else if (barcoTres.horizontal == false){
         barco3V();
     }
 }
-void barco2VIEW(Barco &barcoDois){
+void barco2VIEW(Barco barcoDois){
     if(barcoDois.horizontal == true){
         barco2H();
     }else if(barcoDois.horizontal == false){
@@ -100,6 +106,10 @@ void barco2VIEW(Barco &barcoDois){
 int main(){
     //system("clear");
     system("CLS");
+cout << "Player 1 turn!" << endl;
+cout << endl << "Press ENTER to continue ";
+std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    system("CLS");
     char board[N][N];
     for(int r=0; r<N; r++){
         for(int c=0;c < N; c++){
@@ -107,8 +117,13 @@ int main(){
         }
     }
     printBoard(board, false );
-    barcoCinco.horizontal = true;
-    barco5VIEW(barcoCinco);
+    cout << endl;
+    
+    
+
+
+   /*barcoCinco.horizontal = false;
+    barco5VIEW(barcoCinco);*/
 
     return 0;
 }
