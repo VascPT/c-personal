073cb1fdffdf05125d41426a7552d29e55e4e92c
Vasco <vascorf2008@gmail.com>
2025-11-09 15:30:22 -0100

    .\JogodaGloria.cpp

--- JogodaGloria.cpp
+++ JogodaGloria.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
+//#include <unistd.h>
 #include <limits>
 using namespace std;
 void clear(){
-    #ifdef WIN32
+    #ifdef _WIN32
         system("CLS");
     #else
         system("clear");
@@ -21,17 +23,18 @@ const int Espera_passg = 20; // casa 20
 string nomeP1;
 string nomeP2;
 
+
 void iniciarCasas(string casas[N]){
     for (int i=0; i<N; i++)
-        casas[i] = '-';
-    casas[Perde_Jogada] = "PJ"; //Perde a vez
-    casas[Volta_tres_casas] = "V3"; //Volta 3 casas
-    casas[Avanca_tres_casas] = "A3"; //Avança 3 casas
-    casas[Joga_novament] = "JN"; //Joga novamente
-    casas[Espera_passg] = "EP"; //Espera passagem do próximo jogador
+        casas[i] = "-";
+    casas[Perde_Jogada -1] = "PJ"; //Perde a vez
+    casas[Volta_tres_casas -1] = "V3"; //Volta 3 casas
+    casas[Avanca_tres_casas -1] = "A3"; //Avança 3 casas
+    casas[Joga_novament -1] = "JN"; //Joga novamente
+    casas[Espera_passg -1] = "EP"; //Espera passagem do próximo jogador
 }
 
-void imprimirCasas(string casas[N]){
+void imprimirCasas(const string casas[N], int posP1, int posP2){
     //linha dos números (1...30)
     for(int i=0 ; i<N;i++){ 
         cout<<(i+1<10?" ":"")<<i+1<<" ";
@@ -40,9 +43,28 @@ void imprimirCasas(string casas[N]){
 
     for(int i=0; i<N; i++){
         string cell = casas[i];
+    
+    bool p1 = (posP1 == i);
+    bool p2 = (posP2 == i);
+    if(p1 && p2){
+        cell = "P*"; //Ambos os jogadores na mesma casa
+    }else if(p1){
+        cell = "P1"; //Casa com Player 1
+    }else if(p2){
+        cell = "P2"; //Casa com Player 2
     }
 
+    if(cell.size()==1){ //garantir largura 2 por casa
+        cell = " " + cell; 
+    }
+    cout << cell << " ";
 }
+    cout << "\n";
+}
+
+
+
+
 void dado(){
     std::srand(std::time(nullptr)); // inicializa o gerador com o tempo atual
     int numero = std::rand() % 6 + 1; // número entre 1 e 6 (como um dado)
@@ -51,4 +73,28 @@ void dado(){
 
 int main(){
     clear();
+    cout << "Selecione o nome para o Player 1!: ";
+    cin >> nomeP1;
+    
+    cout << endl << "Ola " << nomeP1 << "! Vamos avancar para o Player 2..." << endl;
+    #ifdef _WIN32
+        Sleep(3000);
+    #else
+        sleep(3);
+    #endif
+    clear();
+    cout << "Selecione o nome para o Player 2!: ";
+    cin >> nomeP2;
+    cout << endl << "Ola " << nomeP2 << "! Prima ENTER para comecar o jogo.";
+    cin.ignore();
+    cin.get();
+    clear();
+
+    string casas[N];
+    iniciarCasas(casas);
+    int posP1 = 0;
+    int posP2 = 0;
+    imprimirCasas(casas, posP1, posP2);
+
+
 }
